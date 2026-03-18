c817eddfcadda9c900aae7d0d4592a356410063f
ablayev1999 <ablayev1999@gmail.com>
2026-03-18 05:43:08 +0500

    update about, contact, localization, responsive styles and fix tours page

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { Plane, Clock, ChevronLeft, ChevronRight } from 'lucide-react';
 import { toursData } from '../data/toursData';
 import './Tours.css';
